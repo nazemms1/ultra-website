90260e09394ace28cb130a42674dfbe5bdf3ab33
n.almsouti <n.almsouti@pharaon-group.com>
2026-07-05 14:22:57 +0300

    fix:performance version 5

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,32 +1,8 @@
-import dynamic from 'next/dynamic'
 import HeroSection from '@/components/Pages/Home/HeroSection'
+import HomeClientSections from '@/components/Pages/Home/HomeClientSections'
 import { getLocale } from 'next-intl/server'
 import { fetchAPI } from '@/lib/api'
 
-// Lazy-load everything below the fold to reduce initial JS bundle
-const ScrollVideoStack = dynamic(() => import('@/components/Pages/Home/ScrollVideoStack'), {
-  ssr: false,
-})
-const PartnersSection = dynamic(() => import('@/components/Pages/Home/Partners'), { ssr: false })
-const Stats = dynamic(() => import('@/components/Pages/Home/Stats/Stats'), { ssr: false })
-const ServicesOrbital = dynamic(
-  () => import('@/components/Pages/Home/Services-orbital/ServicesOrbital'),
-  { ssr: false },
-)
-const Projects = dynamic(() => import('@/components/Pages/Home/Projects/Projects'), { ssr: false })
-const Methodologies = dynamic(
-  () => import('@/components/Pages/Home/Methodologies/Methodologies'),
-  { ssr: false },
-)
-const TestimonialsSection = dynamic(
-  () => import('@/components/Pages/Home/TestimonialsSection'),
-  { ssr: false },
-)
-const CTASection = dynamic(() => import('@/components/Pages/Home/CTASection'), { ssr: false })
-const FAQSection = dynamic(() => import('@/components/Pages/Home/Faqs'), { ssr: false })
-const ContactSection = dynamic(() => import('@/components/Pages/Home/ContactSection'), {
-  ssr: false,
-})
 export default async function HomePage() {
   const locale = await getLocale()
 
@@ -59,29 +35,18 @@ export default async function HomePage() {
   return (
     <>
       <HeroSection data={heroData} />
-      <ScrollVideoStack>
-        {partnersData?.is_shown !== false && (
-          <div id="about">
-            <PartnersSection data={partnersData} />
-          </div>
-        )}
-        {servicesData?.is_shown !== false && (
-          <div id="services">
-            <ServicesOrbital data={servicesData} />
-          </div>
-        )}
-        <Stats data={statsData} />
-        {portfoliosData?.is_shown !== false && (
-          <div id="projects">
-            <Projects data={portfoliosData} />
-          </div>
-        )}
-        <Methodologies data={methodologiesData} />
-      </ScrollVideoStack>
-      <TestimonialsSection data={reviewsData} />
-      <CTASection data={ctaData} />
-      <FAQSection data={faqsData} stillHaveQuestionsData={stillHaveQuestionsData} />
-      <ContactSection data={contactUsData} />
+      <HomeClientSections
+        partnersData={partnersData}
+        servicesData={servicesData}
+        statsData={statsData}
+        portfoliosData={portfoliosData}
+        methodologiesData={methodologiesData}
+        reviewsData={reviewsData}
+        ctaData={ctaData}
+        faqsData={faqsData}
+        stillHaveQuestionsData={stillHaveQuestionsData}
+        contactUsData={contactUsData}
+      />
     </>
   )
 }
---
+++ src/components/Pages/Home/HomeClientSections.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const ScrollVideoStack = dynamic(() => import('@/components/Pages/Home/ScrollVideoStack'), {
+  ssr: false,
+})
+const PartnersSection = dynamic(() => import('@/components/Pages/Home/Partners'), { ssr: false })
+const Stats = dynamic(() => import('@/components/Pages/Home/Stats/Stats'), { ssr: false })
+const ServicesOrbital = dynamic(
+  () => import('@/components/Pages/Home/Services-orbital/ServicesOrbital'),
+  { ssr: false },
+)
+const Projects = dynamic(() => import('@/components/Pages/Home/Projects/Projects'), { ssr: false })
+const Methodologies = dynamic(
+  () => import('@/components/Pages/Home/Methodologies/Methodologies'),
+  { ssr: false },
+)
+const TestimonialsSection = dynamic(
+  () => import('@/components/Pages/Home/TestimonialsSection'),
+  { ssr: false },
+)
+const CTASection = dynamic(() => import('@/components/Pages/Home/CTASection'), { ssr: false })
+const FAQSection = dynamic(() => import('@/components/Pages/Home/Faqs'), { ssr: false })
+const ContactSection = dynamic(() => import('@/components/Pages/Home/ContactSection'), {
+  ssr: false,
+})
+
+interface Props {
+  partnersData: any
+  servicesData: any
+  statsData: any
+  portfoliosData: any
+  methodologiesData: any
+  reviewsData: any
+  ctaData: any
+  faqsData: any
+  stillHaveQuestionsData: any
+  contactUsData: any
+}
+
+export default function HomeClientSections({
+  partnersData,
+  servicesData,
+  statsData,
+  portfoliosData,
+  methodologiesData,
+  reviewsData,
+  ctaData,
+  faqsData,
+  stillHaveQuestionsData,
+  contactUsData,
+}: Props) {
+  return (
+    <>
+      <ScrollVideoStack>
+        {partnersData?.is_shown !== false && (
+          <div id="about">
+            <PartnersSection data={partnersData} />
+          </div>
+        )}
+        {servicesData?.is_shown !== false && (
+          <div id="services">
+            <ServicesOrbital data={servicesData} />
+          </div>
+        )}
+        <Stats data={statsData} />
+        {portfoliosData?.is_shown !== false && (
+          <div id="projects">
+            <Projects data={portfoliosData} />
+          </div>
+        )}
+        <Methodologies data={methodologiesData} />
+      </ScrollVideoStack>
+      <TestimonialsSection data={reviewsData} />
+      <CTASection data={ctaData} />
+      <FAQSection data={faqsData} stillHaveQuestionsData={stillHaveQuestionsData} />
+      <ContactSection data={contactUsData} />
+    </>
+  )
+}
