b8635e13ec7993c5432859d6a94e7c1bcf877ebe
Nazem Almsouti <nazem.msouti@gmail.com>
2026-07-13 14:12:44 +0300

    refactor: remove experimental package imports optimization from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
 const nextConfig: NextConfig = {
-   experimental: {
-    optimizePackageImports: [
-      '@mui/material',
-      '@mui/icons-material',
-      'framer-motion',
-      'lucide-react',
-    ],
-  },
+  
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
