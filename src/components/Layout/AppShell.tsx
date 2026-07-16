179fde453ef3d053eee0f5f2310844d9ab357ce6
Nazem Almsouti <nazem.msouti@gmail.com>
2026-07-16 16:20:12 +0300

    final

--- src/components/Layout/AppShell.tsx
+++ src/components/Layout/AppShell.tsx
@@ -36,6 +36,7 @@ export default function AppShell({
           color: 'text.primary',
           overflowX: { xs: 'hidden', md: 'visible' },
         }}
+        
       >
         <Navbar labels={navLabels} sectionsVisibility={navSectionsVisibility} />
         <Box component="main" sx={{ flex: 1, pb: 6, width: '100%', overflowX: { xs: 'hidden', md: 'visible' } }}>
